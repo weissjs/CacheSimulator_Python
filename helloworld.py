1e49eea9a6076ac599a6e9b2a238d90970d21a53
Jordan Weiss <weissjs@vcu.edu>
2019-11-17 23:33:36 -0500

    adding file in

---
+++ helloworld.py
@@ -0,0 +1,31 @@
+import statistics
+import sys
+
+block_size = sys.argv[1]
+num_blocks = sys.argv[2]
+associativity = sys.argv[3]
+
+print("block size: " + block_size)
+print("Number of Blocks: " + num_blocks)
+print("Associativity: " + associativity)
+
+
+
+# data = [x,y,z];
+#
+# def print_data(data):
+#   print("data is :" + data)
+#
+# def print_it():
+#   for i in data:
+#     print_data(i)
+#
+# print_it()
+#
+# x = "hello"
+# y = "world"
+# z = "today"
+#
+# data = [x,y,z]
+#
+# print_it()
