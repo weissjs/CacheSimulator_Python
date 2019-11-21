e29cba7e5738bc6970ee849111edcb1ceec22250
unknown <weissjs@vcu.edu>
2019-11-21 17:31:12 -0500

    anthonys commit

--- helloworld.py
+++ helloworld.py
@@ -14,8 +14,10 @@ class cache:
         self.offset_bits = math.ceil(math.log2(block_size))
         self.index_bits = math.ceil(math.log2(num_blocks))
         self.tag_bits = 32 - self.offset_bits - self.index_bits
-    def printCache(self):
-        for
+    #@property
+    def printCache():
+        for x in tags:
+            print("index is: ", x, "yah")
 
     def placeAddress(self):
         print(blank)
@@ -30,13 +32,19 @@ class test:
                 self.input_arr.append(int(myline, 16))
                 i = i + 1
 
+def main():
+    c1 = cache(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+    print("block size: ", c1.block_size)
+    print("number of blocks: ", c1.num_blocks)
+    print("Associativity: ", c1.associativity)
+    print("offset bit #: ", c1.offset_bits)
+    print("index bit #: ", c1.index_bits)
+    print("tag bit #: ", c1.tag_bits)
 
-c1 = cache(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
-print("block size: ", c1.block_size)
-print("number of blocks: ", c1.num_blocks)
-print("Associativity: ", c1.associativity)
-print("offset bit #: ", c1.offset_bits)
-print("index bit #: ", c1.index_bits)
-print("tag bit #: ", c1.tag_bits)
-test1 = test(sys.argv[4]).input_arr
-print(test1)
+    test1 = test(sys.argv[4])
+
+    print(test1.input_arr)
+    test1.printCache;
+
+if __name__ == '__main__':
+    main()
