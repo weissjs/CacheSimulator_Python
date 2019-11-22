a9363d9c485cd71bff25488c017483f1f4f80627
unknown <weissjs@vcu.edu>
2019-11-22 10:15:12 -0500

    big boi doneski

--- helloworld.py
+++ helloworld.py
@@ -19,6 +19,7 @@ class cache:
         self.addresses = [0] * num_blocks
         self.set_num = num_blocks / N
         self.numCacheLines =  math.ceil((self.num_blocks * self.block_size) / (self.block_size * N))
+        self.set_addresses =  [[0 for x in range(0, self.N)] for x in range(0, self.numCacheLines)]
 
         self.set_tags = [[0 for x in range(0, self.N)] for x in range(0, self.numCacheLines)]
         self.set_valid = [[0 for x in range(0,self.N)] for x in range(0, self.numCacheLines)]
@@ -32,11 +33,17 @@ class cache:
         index = math.floor(address / self.block_size) % self.num_blocks
 
         tag = address >> (self.index_bits + self.offset_bits)
+
+        # print("placing: ", address)
+        # print("index: ", index)
+        # print("tag:  ", tag)
+        # print("number of cache lines: ", self.numCacheLines)
+
         if self.tags[index] == tag:
             if self.valid[index] == 1:
                 self.hit_count += 1
             else:
-                #self.tags[index] = tag
+                self.tags[index] = tag
                 self.valid[index] = 1
                 self.miss_count += 1
         else:
@@ -44,43 +51,57 @@ class cache:
             self.valid[index] = 1
             self.miss_count += 1
 
-            self.addresses[index] = address
+        self.addresses[index] = address
 
     def placeAddressAssociative(self, address):
         index = math.floor(address / self.block_size) % self.num_blocks
 
 
-        set_num = math.floor(address / self.block_size) % (self.numCacheLines + 1)
+        set_num = math.floor(address / self.block_size) % (self.numCacheLines)
         tag = int(address / (self.numCacheLines * self.block_size))
 
-        if(tag in self.set_tags[set_num]):
-            position = self.set_tags[set_num].index(tag)
-            if(self.set_valid[set_num][position]):
-                self.hit_count += 1
-            else:
-                self.set_valid[set_num][position]
-                self.miss_count += 1
+        # print("placing: ", address)
+        # print("index: ", index)
+        # print("tag:  ", tag)
+        # print("set number: ", set_num)
+        # print("number of cache lines: ", self.numCacheLines)
+        flag = 0
+        if(tag in self.set_tags[set_num] and self.set_valid[set_num][self.set_tags[set_num].index(tag)]):
+            self.set_addresses[set_num][self.set_tags[set_num].index(tag)] = address
+            # print("hit")
+            self.hit_count += 1
+
         else:
-            if(0 in self.set_valid[set_num]):
-                position = self.set_valid[set_num].index(0)
-                self.set_tags[set_num][position] = tag
-                self.set_valid[set_num][position] = 1
-                self.miss_count += 1
-            else:
+            for x in range(0, self.N):
+                if(self.set_valid[set_num][x] == 0):
+                    position = self.set_valid[set_num].index(0)
+                    self.set_tags[set_num][x] = tag
+                    self.set_valid[set_num][x] = 1
+                    self.miss_count += 1
+                    # print("miss at 2")
+                    self.set_addresses[set_num][position] = address
+                    flag = 1
+                    break
+            if(flag == 0): # does not find open spot
+                position = 0
                 self.set_tags[set_num][0] = tag
                 self.set_valid[set_num][0] = 1
                 self.miss_count += 1
-
+                # print("miss at 3")
+                self.set_addresses[set_num][position] = address
 
     def printAssCache(self):
         i = 0
         while(i < self.numCacheLines):
-            for x in self.set_tags[i]:
-                position = self.set_tags[i].index(x)
-                {print("set: ", i, "position: ", position, "valid: ",
-                    self.set_valid[i][position], "tag: ",
-                    self.set_tags[i][position])}
+            x = 0
+            while(x < self.N):
+                {print("set: ", i, "position: ", x, "valid: ",
+                    self.set_valid[i][x], "tag: ",
+                    self.set_tags[i][x], "start memory: ",
+                    self.set_addresses[i][x])}
+                x+=1
             i+=1
+        print()
 
     def printCache(self):
         i = 0
@@ -88,6 +109,7 @@ class cache:
             print("index is: ", i, "valid: ", self.valid[i], "tag: ",
                     self.tags[i], "start mem: ", self.addresses[i])
             i+=1
+        print()
 
 
 class test:
@@ -101,31 +123,26 @@ class test:
 
 def main():
     c1 = cache(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
-
-
     test1 = test(sys.argv[4])
 
-    print(test1.input_arr)
-    c1.printAssCache()
-    c1.placeAddressAssociative(0)
-    c1.printAssCache()
-    c1.placeAddressAssociative(3)
-    c1.printAssCache()
-    c1.placeAddressAssociative(11)
-    c1.printAssCache()
-    c1.placeAddressAssociative(16)
-    c1.printAssCache()
-    c1.placeAddressAssociative(21)
-    c1.printAssCache()
-    c1.placeAddressAssociative(11)
-    c1.printAssCache()
-    c1.placeAddressAssociative(16)
-    c1.printAssCache()
-    c1.placeAddressAssociative(48)
-    c1.printAssCache()
-    c1.placeAddressAssociative(16)
-    c1.printAssCache()
-    c1.placeAddressAssociative(0)
+    if(c1.N < 2):
+        i = 0
+        while i < len(test1.input_arr):
+            c1.placeAddressDirect(test1.input_arr[i])
+            #c1.printCache()
+            i+=1
+        c1.printCache()
+    else:
+        i = 0
+        while i < len(test1.input_arr):
+            c1.placeAddressAssociative(test1.input_arr[i])
+            #c1.printAssCache()
+            i+=1
+        c1.printAssCache()
+
+    total = c1.miss_count + c1.hit_count
+    miss_rate = (c1.miss_count / total) * 100
+    hit_rate = (c1.hit_count / total) * 100
 
     print("block size: ", c1.block_size)
     print("number of blocks: ", c1.num_blocks)
@@ -135,5 +152,8 @@ def main():
     print("tag bit #: ", c1.tag_bits)
     print("miss count: ", c1.miss_count)
     print("hit count: ", c1.hit_count)
+    print("total: ", total)
+    print("miss rate: ", miss_rate, "%")
+    print("hit rate: ", hit_rate, "%")
 if __name__ == '__main__':
     main()
